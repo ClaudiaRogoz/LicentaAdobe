cab9b566a79770c5a63a0133bf24591ac7dcf3e2
crogoz <crogoz@crogoz-osx.corp.adobe.com>
2016-03-21 17:40:43 +0200

    removed some files

--- DerivedData/XMLParser/Build/Intermediates/XMLParser.build/Debug/XMLParser.build/Objects-normal/x86_64/XMLReader.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/crogoz/Desktop/XMLParser/XMLParser/XMLReader.m \
-  /Users/crogoz/Desktop/XMLParser/XMLParser/XMLReader.h
--- DerivedData/XMLParser/Build/Intermediates/XMLParser.build/Debug/XMLParser.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/crogoz/Desktop/XMLParser/XMLParser/main.m \
-  /Users/crogoz/Desktop/XMLParser/XMLParser/XMLReader.h
