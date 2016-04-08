5b8bd4041a16f55455fd9c02c3de50816dccafda
crogoz <crogoz@crogoz-osx.corp.adobe.com>
2016-04-08 17:40:33 +0300

    TODO:merge dict + toString

--- XMLParser/XMLGenerator.m
+++ XMLParser/XMLGenerator.m
@@ -7,12 +7,15 @@
 //
 
 #import "XMLGenerator.h"
+
 #define DEF_PATH @"/Users/crogoz/Desktop/XMLParser/XMLParser/Defs.json"
 #define RULES_PATH @"/Users/crogoz/Desktop/XMLParser/XMLParser/Rules.json"
 #define TEST_PATH @"/Users/crogoz/Desktop/XMLParser/XMLParser/TestGenerator.json"
+
 #define RANDOM @"$rand"
-
-
+#define SCENE @"$sceneNo"
+#define DEFAULT @"default"
+#define ARTBOARDSCENE @"$artboardsceneNo"
 
 @implementation XMLGenerator
 
@@ -40,6 +43,7 @@
     agcToXmlTemplate = defDict;
     translationDict = ruleDict;
     uniqueIds = [[NSMutableDictionary alloc] init];
+    sceneNo = 0;
 
 
 }
@@ -69,18 +73,107 @@
 
 }
 
--(void) computeObjects:(NSString *)rule condition:(NSString*)cond params:(NSDictionary *)dict {
+// TODO generate toString depending on
+-(NSString *) toString:(NSMutableDictionary *)dict name:(NSString*)varName {
+    NSArray *order = [dict objectForKey:@"toString"];
+    
+    NSMutableString *tagStr = [[NSMutableString alloc] init];
+   
+    for (id object in order) {
+        
+    }
+    
+    return @"";
+}
+
+-(void) mergeDefaultValues:(NSDictionary*)defaultDict withDict:(NSMutableDictionary **) initDict usingDict:(NSDictionary*) paramDict {
+    
+    NSLog(@"Trying to merge %@ with %@", defaultDict, *initDict);
+    for (id key in [defaultDict allKeys]) {
+        NSString *value = [defaultDict objectForKey:key];
+        [*initDict setObject:value forKey:key];
+    }
+    
+    
+    NSString *dependency = [[paramDict allKeys] objectAtIndex:0];
+    paramDict = [paramDict objectForKey:dependency];
+    NSLog(@"Trying to merge %@ with %@", paramDict, *initDict);
+    for (id key in [*initDict allKeys]) {
+        NSString *value = [*initDict objectForKey:key];
+        NSLog(@"keyK = %@ %@", key, value);
+        BOOL toTransform = [value isKindOfClass:[NSString class]] && [value hasPrefix:@"$"];
+        if (toTransform) {
+            NSLog(@"transform %@", key);
+            [*initDict setObject:[paramDict objectForKey:key] forKey:key];
+        }
+    }
+    //TODO? maybe we need to propagate pairs from paramDict <maybe ?!> => then "toString" MUST be rewritten!!!
+
+}
+
+-(NSMutableDictionary *) computeObjects:(NSString *)rule condition:(NSString*)cond params:(NSDictionary *)dict agcDict:agcParams{
+    
     NSMutableDictionary *objDict = [[[agcToXmlTemplate objectForKey:@"subviews"] objectForKey:rule] mutableCopy];
     
     
+    // now changing based on params
+    if (!cond) {
+        //just generate the tag; no other transformations are needed
+        return objDict;
+        
+    }
+    //get values from agc in order to transfer them into xml
+    id values = agcParams;
+    NSArray *goToAgc = [self splitVariable:cond];
     
-    
+    for (id key in goToAgc) {
+        
+        id nodeValue;
+        NSLog(@"Check for key = %@", key);
+        if ([key hasPrefix:@"$"] && [key isEqualToString:SCENE]) {
+            nodeValue = [values objectAtIndex:sceneNo];
+        
+        } else if ([key hasPrefix:@"$"] && [key isEqualToString:ARTBOARDSCENE]) {
+            NSString *artboard = [NSString stringWithFormat:@"artboard%d", sceneNo +1];
+            nodeValue = [values objectForKey:artboard];
 
+        } else {
+            nodeValue = [values objectForKey:key];
+            
+        }
+        
+        values = nodeValue;
+        NSLog(@"TempV = %@", values);
+    }
+    
+    NSMutableDictionary *dictValue = values;
+    BOOL isEmpty = ([dictValue count] == 0);
+    if (isEmpty && !dictValue) {
+        //TODO use default values!!!
+        NSLog(@"IT is null");
+        [self mergeDefaultValues:[objDict objectForKey:DEFAULT] withDict:&objDict usingDict:dict];
+        NSLog(@"t0 merge = %@", objDict);
+        return nil;
+    } else if (isEmpty) {
+        //no subview found
+        NSLog(@"it is empty");
+        return nil;
+    } else {
+        //TODO check type 08.03.2016
+        // based on type look at rules.json -> gotoSubviews..
+        //maybe recursive ?
+        //merge "dict" with "values"
+        NSLog(@"NOT NULL TODO\n");
+        
+    }
+    
+    NSLog(@"ValuesForKey = %@", values);
+    return values;
 
 }
 
 
--(NSDictionary*) processWholeXmlFile {
+-(NSDictionary*) processWholeXmlFromAgc:(NSDictionary *)agcDict {
     
     NSMutableDictionary *finalDict = [NSKeyedUnarchiver unarchiveObjectWithData:[NSKeyedArchiver archivedDataWithRootObject: [agcToXmlTemplate objectForKey:@"content"]]];
     NSLog(@"finalDict = %@ %@", finalDict, agcToXmlTemplate);
@@ -94,11 +187,21 @@
         
         
         if ([keys count] == 1) {
+            
             //TODO goto "subviews" dictionary
             NSLog(@"TO Process %@", rule);
             NSMutableDictionary *rulesDict = [rulesTempDict objectForKey:rule];
-            NSString* cond = [[rulesDict allKeys] objectAtIndex:0];
-            [self computeObjects:rule condition:cond params:[rulesTempDict objectForKey:rule]];
+            if ([rulesDict isKindOfClass:[NSArray class]])
+                continue;
+            NSArray *conds = [rulesDict allKeys];
+            NSString* cond;
+            if ([rulesDict count] == 0)
+                cond = nil;
+            else
+                cond = [[rulesDict allKeys] objectAtIndex:0];
+            
+            [self computeObjects:rule condition:cond params:[rulesTempDict objectForKey:rule] agcDict:agcDict];
+        
         } else {
             //TODO the modified variable is in the "finalDict"
             
@@ -121,7 +224,7 @@
     if (!agcObject) {
         // it was given the whole dictionary to process => goto @"content"; type = "view"
         // TODO insert header + footer of the xml file
-                return [self processWholeXmlFile];
+        return [self processWholeXmlFromAgc:typeAgcObject];
         
     }
     // look at subviews
