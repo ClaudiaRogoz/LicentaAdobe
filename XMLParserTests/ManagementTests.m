8566f9f0baa95393e1a5b63a18f888d58e30bf8a
crogoz <crogoz@adobe.com>
2016-06-26 13:35:58 +0300

    started unit testing

---
+++ XMLParserTests/ManagementTests.m
@@ -0,0 +1,37 @@
+//
+//  ManagementTests.m
+//  XDXCodeTranslator
+//
+//  Created by crogoz on 26/06/16.
+//  Copyright © 2016 crogoz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "XCode2XD.h"
+#import "Dict2Agc.h"
+#import "Xml2Dict.h"
+
+@interface ManagementTests : XCTestCase
+
+@end
+
+@implementation ManagementTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCTAssert(true, "Pass");
+}
+
+@end
